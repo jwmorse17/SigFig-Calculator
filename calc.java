8ed263bd250cac8658d12107e776a5f7f9c0df77
Joshua Morse <jwmorse@unm.edu>
2019-09-28 15:34:09 -0600

    Parse: not working yet.
    Added convertToInteger method which changes the char value passed to a Integer type
    
    Added performCalc method, but does not perform the function yet

--- calc.java
+++ calc.java
@@ -30,6 +30,11 @@ public class calc {
 		calc object = new calc();
 
 	}
+	/**
+	 * A method that takes user input from the console
+	 * @param void
+	 * @return void
+	 */
 
 	public void userInput(){
 		System.out.println("Enter your euqation");
@@ -40,11 +45,19 @@ public class calc {
 	}
 
 
-
-	public String parse(String eq){
+	/**
+	 * A method that reads in a string and parses it such that integers are added to the numbers
+	 * collection, operators are added to the operators collection
+	 * TODO:// decide if the return needs to be changed
+	 * @param eq the string of the input from the user. 
+	 * 
+	 */
+	public void parse(String eq){
 		int opCounter = 0;
 		int intCounter = 0;
 		StringBuilder sb = new StringBuilder(eq);
+
+		//first remove operators and store in operators collection
 		for(int i = 0; i < sb.length(); i++){
 			char currentChar = sb.charAt(i);
 			if(currentChar == '+' || currentChar == '/' || currentChar == '*' || currentChar == '-'){
@@ -56,28 +69,49 @@ public class calc {
 			System.out.println("\n" + sb);
 			///// end testing//////
 		}
+		System.out.println("\n length = " + sb.length());
 
 		//StringBuilder sb1 = new StringBuilder(sb);
 		for(int i = 0; i < sb.length(); i++){
+			int spcCntr = 0;
 			char currentChar = sb.charAt(i);
-			while(currentChar != ' ' || i > sb.length()){
-				StringBuilder sb2 = new StringBuilder();
-				sb2.append(currentChar);
-				i++;
-			}
-			System.out.println("\n" + sb2);
+			//number could be any length, but is always seperated by a space
 
-/*			System.out.println(i);
-			char currentChar = sb.charAt(i);
-			if(i == 0){
-				StringBuilder sb2 = new StringBuilder();
+			//TODO: find a better way to do this. 
+			while(currentChar != ' ' || i >= sb.length()){
+				System.out.println("\n Current Char start of while " + currentChar);
+				//StringBuilder sb2 = new StringBuilder();
+				//sb2.append(currentChar);
+				sb.deleteCharAt(i);
+				//i++;
+				currentChar = sb.charAt(i);
+				System.out.println("\n Current Char end of while " + currentChar + "12315");
 			}
-			if(currentChar == ' '){
-				stringNums.add(sb2);
-				StringBuilder sb2 = new StringBuilder();
-			sb2.append(currentChar);*/
+
+			//space denotes end of current number
+			if(currentChar == ' ' || i >= sb.length()){
+				spcCntr++;
+				numbers.put(spcCntr, convertToInteger(currentChar));
+			}
 		}
 
-		return null;
+		performCalc();
+
+	}
+
+
+	public Integer convertToInteger(char currentChar){
+		return Character.getNumericValue(currentChar);
+	}
+
+	/**
+	 * A method that performs the calculations on the string that has been parsed
+	 */ 
+	public void performCalc(){
+
+
+		int calc = numbers.get(1);
+		System.out.println(calc);
+
 	}
 }
